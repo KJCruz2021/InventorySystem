0ba592cdbdf34c2dc725504cc8b84b10143c026e
avinpogi123 <105871711+avinpogi123@users.noreply.github.com>
2023-11-21 00:45:15 +0800

    qweqweqeq
    asdsadsadsadqw

--- TakoyakenSystem/Inventory/urls.py
+++ TakoyakenSystem/Inventory/urls.py
@@ -1,12 +1,13 @@
-
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 #dito ung parang paths / url nila
 #nakaconnect to sa views.py (kung ano mga function don, dapat nakalagay din dto)
 
-urlpatterns = {
+urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.login, name='login'),  
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-}
+]
