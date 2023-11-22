db0fdbfaf0a74c88fc14da5c9cbdfcc9387f7316
zShieda <zillohpunzalan29@gmail.com>
2023-11-22 22:23:58 +0800

    signup
    views.py

--- TakoyakenSystem/Inventory/views.py
+++ TakoyakenSystem/Inventory/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+
 # Create your views here.
 
 #dito lalagay ung mga bagong page ng html
 
 def login(request):
+    
     return render(request, 'login.html', {})
 
 def signup(request):
@@ -16,7 +18,7 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
         
-        myuser = User.objects.create_user(email, pass1)
+        myuser = User.objects.create_user(email,pass1)
         myuser.email = email
         myuser.password = pass1
         myuser.save()
