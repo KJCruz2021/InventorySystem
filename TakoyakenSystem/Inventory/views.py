a5a06a71b2c617d7b7c2da6471a9285da1e12993
zShieda <zillohpunzalan29@gmail.com>
2023-11-22 21:36:56 +0800

    update: signup

--- TakoyakenSystem/Inventory/views.py
+++ TakoyakenSystem/Inventory/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 #dito lalagay ung mga bagong page ng html
@@ -8,6 +10,18 @@ def login(request):
     return render(request, 'login.html', {})
 
 def signup(request):
+
+    if request.method == "POST":
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        myuser = User.objects.create_user(email, pass1)
+        myuser.email = email
+        myuser.password = pass1
+        myuser.save()
+
+        return redirect('signup')
     return render(request, 'signup.html', {})
 
 def inventory(request):
